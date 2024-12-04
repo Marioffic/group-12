ae409efc2dc4188563e9d7d45026f9a35846b8e8
Marion Office <87404335+Marioffic@users.noreply.github.com>
2024-12-04 15:14:07 -0600

    altered main so that only one instance of the dahsboard can be opened

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,30 +1,65 @@
 package application;
-	
-import javafx.application.Application;
 
+import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-	    try {
-	        FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
-	        AnchorPane root = loader.load();
-	        Scene scene = new Scene(root);
-	        primaryStage.setScene(scene);
-	        primaryStage.setTitle("Farm Dashboard");
-	        primaryStage.show();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	}
 
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    // Static variable for holding the single instance of the dashboard1
+    private static Main instance;
+    private static Stage dashboardStage;
+
+    public Main() {
+        // Prevent instantiation of multiple Main objects
+        if (instance != null) {
+            throw new IllegalStateException("Main instance already created");
+        }
+        instance = this;
+    }
+
+    // Getter for the Singleton instance
+    public static Main getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        if (dashboardStage != null) {
+            // If the dashboard1 is already open, bring it to the front
+            dashboardStage.toFront();
+            return;
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
+            AnchorPane root = loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Farm Dashboard");
+
+            // Store the stage instance to enforce the Singleton behavior
+            dashboardStage = primaryStage;
+            primaryStage.setOnCloseRequest(event -> dashboardStage = null);
+
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    // Method to ensure the dashboard1 can be shown only once
+    public void showDashboard() {
+        if (dashboardStage != null) {
+            dashboardStage.toFront();
+        } else {
+            start(new Stage());
+        }
+    }
 }
